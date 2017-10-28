943eb607f1c7ff05ed9d860bbebe3c1afadc1ed8
codemodeco <codemode.co@gmail.com>
2017-10-28 07:04:10 +0530

    added ssl

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var schema = (req.headers['x-forwarded-proto'] || '').toLowerCase();
+  if (schema === 'https') {
   res.render('index', { title: 'Express' });
+  } else {
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+  
 });
 
 module.exports = router;
