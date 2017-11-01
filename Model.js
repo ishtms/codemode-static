07ebebe28728fa4a8bd8677e23c8f009b9fce9e1
codemodeco <codemode.co@gmail.com>
2017-11-02 00:36:48 +0530

    finalized

--- Model.js
+++ Model.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose')
 
 var Schema = mongoose.Schema({
-    user: {type: Boolean, default: true}
+    user: {type: Boolean, default: true},
+    time: {type: String, default: new Date().getDate() +" "+ new Date().getDay()+" " + new Date().getTime() + " "+ new Date().getFullYear()}
 })
 
 module.exports = mongoose.model('UserCount', Schema)
