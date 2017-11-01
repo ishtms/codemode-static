703a2ce0f151566352d16155a850ef04cfe2efb3
codemodeco <codemode.co@gmail.com>
2017-11-02 00:40:01 +0530

    addd user count

--- Model.js
+++ Model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 
 var Schema = mongoose.Schema({
     user: {type: Boolean, default: true},
-    time: {type: String, default: new Date().toDateString()
+    time: {type: String, default: new Date().toDateString()}
 })
 
 module.exports = mongoose.model('UserCount', Schema)
